32aff529778da6e38ee91075376ef751ca9ba036
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-05-19 08:06:15 +0200

    Create Erastotene_method2.py

---
+++ Erastotene_method2.py
@@ -0,0 +1,3 @@
+'''
+Il programma calcola la posizione di Giove, Saturno, Urano e Nettuno rispetto all'osservatore terrestre per un intervallo di tempo di un mese (dal 1 aprile al 1 maggio 2023) e traccia un grafico della loro longitudine apparente rispetto
+'''
